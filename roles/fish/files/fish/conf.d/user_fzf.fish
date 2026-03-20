f93a17c6a39a7a2911837808789c7fbbfd476c39
Sergei Aleksandrov <sergei.a.aleks@gmail.com>
2026-03-20 20:11:48 +0100

    fix(fish): Change fish keybindings

--- roles/fish/files/fish/conf.d/user_fzf.fish
+++ roles/fish/files/fish/conf.d/user_fzf.fish
@@ -7,4 +7,8 @@ set -l c2 '--color=fg:#9DA0A2,header:#DF5273,info:#FFB987,pointer:#000000'
 set -l c3 '--color=marker:#24A8B4,fg+:#000000,prompt:#EFB993,hl+:#DF5273'
 set -l c4 '--color=fg+:regular,hl:bold,hl+:bold,pointer:bold,marker:bold'
 
-set -U FZF_DEFAULT_OPTS $c0 $c1 $c2 $c3 $c4
+# Colors
+set -gx FZF_DEFAULT_OPTS $c0 $c1 $c2 $c3 $c4
+
+# Keybindings
+set -gx FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS "--bind" "ctrl-delete:kill-word"
--- roles/fish/files/fish/functions/fish_user_key_bindings.fish
+++ roles/fish/files/fish/functions/fish_user_key_bindings.fish
@@ -3,26 +3,29 @@ function fish_user_key_bindings
   bind \el 'user_list_current_token'
 
   # ctrl+shift+f
-  bind \e\[70\;5\;6\~ 'echo "Ctrl+Shift+F"'
+  bind \e\[70\;6u 'br --cmd "//&cr//i"'
 
   # ctrl+g
   bind \cg '__fzf_find_file'
+
   # ctrl+shift+g
-  bind \e\[71\;5\;6\~ '__fzf_cd --hidden'
+  bind \e\[71\;6u '__fzf_cd --hidden'
 
   # ctrl+r
   bind \cr '__fzf_reverse_isearch'
+
   # ctrl+shift+r
-  bind \e\[72\;5\;6\~ 'user_fzf_select_z'
+  bind \e\[82\;6u 'user_fzf_select_z'
 
   # ctrl+delete
   bind \e\[3\;5~ kill-word
   # ctrl+backspace
   bind \b backward-kill-word
 
+  # ctrl+e
   bind \ce 'br .'
 
-  # bind \e\[1\;3B "prevd; commandline -f repaint"
-  # bind \e\[1\;3A "nextd; commandline -f repaint"
+  # ctrl+h
+  bind \ch 'cd ~; commandline -f repaint'
 
 end
