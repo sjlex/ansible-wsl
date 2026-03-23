82e6c96ed179981ee1143769d23843e7174dc14a
Sergei Aleksandrov <sergei.a.aleks@gmail.com>
2026-03-23 11:57:54 +0100

    refactor(fish): Keybindings

--- roles/fish/files/fish/init.fish
+++ roles/fish/files/fish/init.fish
@@ -6,6 +6,9 @@ end
 # Load environment variables
 source $__fish_config_dir/env.fish
 
+# Load keybindings
+source $__fish_config_dir/keybindings.fish
+
 # Load aliases
 for alias in $__fish_config_dir/aliases/**/*
   source $alias
